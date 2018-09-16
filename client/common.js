3dccd5bfaa41a5fa205ad4c1a3fcb5447cad4895
Shari Cahill <sharicahill@gmail.com>
2018-09-17 06:09:16 +1000

    Initial commit

---
+++ client/common.js
@@ -0,0 +1,11 @@
+Meteor.startup(function () {
+  AccountsEntry.config({
+    homeRoute: '/',
+    dashboardRoute: '/',
+    passwordSignupFields: 'USERNAME_AND_EMAIL'
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_AND_EMAIL'
+  });
+});
